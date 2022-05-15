687f17aafb8434ad322e055dd36eb6fe2302bc10
kou12345 <hi.0412.ko.2002@gmail.com>
2022-05-16 03:36:34 +0900

    ABC081A の解答コードを追加

---
+++ coder/AtcoderBeginnersSelection/ABC081A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s = "";
+    cin >> s;
+    int count = 0;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '1')
+        {
+            count++;
+        }
+        
+    }
+    
+    cout << count << endl;
+
+	return 0;
+}
