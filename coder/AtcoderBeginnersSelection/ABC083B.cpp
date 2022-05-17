9843ad7576264c2307098ec37ec36ad99a09d168
kou12345 <hi.0412.ko.2002@gmail.com>
2022-05-17 10:28:38 +0900

    ABC083B のコードを追加　まだ解き途中

---
+++ coder/AtcoderBeginnersSelection/ABC083B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int N, A, B;
+   int ans = 0;
+//    cin >> N >> A >> B;
+
+    cout << 1 % 10 << endl;
+    cout << 10 % 10 << endl;
+    cout << 123 % 10 << endl;
+    cout << 123 % 100 << endl;
+
+//    for (int i = 0; i < N; i++)
+//    {
+//        if(i <= A && i >= B)
+//        {
+//            ans += i;
+//        }
+//    }
+
+//    cout << ans << endl;
+
+    // 桁数を取得
+    // 桁数の数だけループ
+    // N % 10
+    // A % 10
+    
+
+    return 0;
+}
