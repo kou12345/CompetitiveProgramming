d554a074d357ed80536ef909a90187cd1cc22046
kou12345 <hi.0412.ko.2002@gmail.com>
2023-08-03 11:07:53 +0900

    ac

---
+++ code_storage/AtCoderBeginnerContest/abc312/a.py
@@ -0,0 +1,20 @@
+S = input()
+
+if S == "ACE" or S == "BDF" or S == "CEG" or S == "DFA" or S == "EGB" or S == "FAC" or S == "GBD":
+    print("Yes")
+else:
+    print("No")
+
+"""
+S = input()
+
+moji = ['ACE', 'BDF', 'CEG', 'DFA', 'EGB', 'FAC', 'GBD']
+
+if S in moji:
+    print("Yes")
+else:
+    print("No")
+
+こっちのほうが良いわ
+    
+"""
