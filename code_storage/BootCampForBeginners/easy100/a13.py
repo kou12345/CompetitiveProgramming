de1ad5c2a15e5d80b87fee64f41a4012cca26bc6
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-23 15:53:10 +0900

    wip

--- code_storage/BootCampForBeginners/easy100/a13.py
+++ code_storage/BootCampForBeginners/easy100/a13.py
@@ -2,23 +2,27 @@
 
 a, b, c = map(int, input().split())
 
+# クッキーを交換した回数
 count = 0
 
-while True:
-    if a == b == c and a % 2 == 0:
-        break
-    if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
-        tmp_a = a / 2
-        tmp_b = b / 2
-        tmp_c = c / 2
+# 全て同じ数字且つ偶数の場合は、break
+if (a == b == c) and (a % 2 == 0):
+    count = -1
+else:
+    while True:
+        # 全てが2で割り切れる時
+        if (a % 2 == 0) and (b % 2 == 0) and (c % 2 == 0):
+            tmp_a = a / 2
+            tmp_b = b / 2
+            tmp_c = c / 2
 
-        a = tmp_b + tmp_c
-        b = tmp_a + tmp_c
-        c = tmp_a + tmp_b
+            a = tmp_b + tmp_c
+            b = tmp_a + tmp_c
+            c = tmp_a + tmp_b
 
-        count += 1
-    else:
-        break
+            count += 1
+        else:
+            break
 
 if count == 0:
     print(-1)
